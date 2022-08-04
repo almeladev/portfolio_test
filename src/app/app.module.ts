ecd1220c953973ea6794eb1ae911081f16910745
D. Almela <hi@almela.dev>
2022-08-05 00:57:45 +0200

    (WIP) test netlify

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,18 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { MarkdownModule } from 'ngx-markdown';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [AppComponent],
   imports: [
     BrowserModule.withServerTransition({ appId: 'serverApp' }),
-    AppRoutingModule
+    AppRoutingModule,
+    MarkdownModule.forRoot(),
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
--- src/app/blog/blog.module.ts
+++ src/app/blog/blog.module.ts
@@ -3,15 +3,10 @@ import { CommonModule } from '@angular/common';
 
 import { BlogRoutingModule } from './blog-routing.module';
 import { IndexComponent } from './index/index.component';
-
+import { MarkdownModule } from 'ngx-markdown';
 
 @NgModule({
-  declarations: [
-    IndexComponent
-  ],
-  imports: [
-    CommonModule,
-    BlogRoutingModule
-  ]
+  declarations: [IndexComponent],
+  imports: [CommonModule, BlogRoutingModule, MarkdownModule.forChild()],
 })
-export class BlogModule { }
+export class BlogModule {}
